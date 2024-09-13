43322cd3d842f7d7e12413219770a20ba4972e20
Onur Aylanç <aylanc.onur@gmail.com>
2024-09-13 22:13:23 +0300

    PD-07: Implementing MessagesService to MessagesController

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,29 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // DON'T DO THIS ON REAL APPS
+    // USE DEPENDENCY INJECTION
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll(); // findAll is a method of MessagesService
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content); // content is a property of CreateMessageDto
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    return this.messagesService.findOne(parseInt(id)); // findOne is a method of MessagesService
   }
 }
